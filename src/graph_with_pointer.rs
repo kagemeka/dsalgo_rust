02bb7114b856a2ec9aedfb31d03cb3dc68e20111
kagemeka <kagemeka1@gmail.com>
2022-02-28 05:43:46 +0000

    fix pointer graph initializer and impl add-undirected_edge

--- src/graph_with_pointer.rs
+++ src/graph_with_pointer.rs
@@ -110,7 +110,9 @@ impl<T, U> Graph<T, U> {
         T: Default,
     {
         Self {
-            nodes: vec![Rc::new(RefCell::new(Node::default())); size],
+            nodes: (0..size)
+                .map(|_| Rc::new(RefCell::new(Node::default())))
+                .collect(),
         }
     }
 
@@ -135,6 +137,21 @@ impl<T, U> Graph<T, U> {
                 self.nodes[to].clone(),
                 data,
             ))));
+    }
+
+    pub fn add_undirected_edge(&mut self, left: usize, right: usize, data: U)
+    where
+        T: 'static,
+        U: 'static,
+    {
+        assert!(left < self.size() && right < self.size());
+        let edge = Rc::new(RefCell::new(UndirectedEdge::new(
+            self.nodes[left].clone(),
+            self.nodes[right].clone(),
+            data,
+        )));
+        self.nodes[left].borrow_mut().edges.push(edge.clone());
+        self.nodes[right].borrow_mut().edges.push(edge.clone());
     }
 }
 
@@ -170,6 +187,8 @@ mod tests {
         let mut graph = super::Graph::<PureNone, usize>::new(2);
         println!("{:?}", graph);
         graph.add_directed_edge(0, 1, 3);
+        println!("{:?}", graph);
+        graph.add_undirected_edge(0, 1, 2);
         println!("{:?}", graph);
     }
 }
