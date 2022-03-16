838e7d4edabd6efeb61f161a07b0d08d0595b10e
kagemeka <kagemeka1@gmail.com>
2022-03-16 02:47:17 +0000

    impl splay

--- src/splay_tree.rs
+++ src/splay_tree.rs
@@ -3,6 +3,7 @@ pub(crate) struct Node<K, V> {
     pub(crate) value: V,
     pub(crate) left: Option<Box<Node<K, V>>>,
     pub(crate) right: Option<Box<Node<K, V>>>,
+    pub(crate) size: usize,
 }
 
 impl<K: PartialOrd, V> Node<K, V> {
@@ -12,59 +13,88 @@ impl<K: PartialOrd, V> Node<K, V> {
             value: value,
             left: None,
             right: None,
+            size: 1,
         })
     }
 
-    
+    pub(crate) fn get_size(root: Option<&Box<Self>>) -> usize {
+        if let Some(node) = root { node.size } else { 0 }
+    }
+
+    pub(crate) fn update(root: &mut Box<Self>) {
+        root.size = Self::get_size(root.left.as_ref()) + Self::get_size(root.right.as_ref()) + 1;
+    }
+
+    pub(crate) fn rotate_left(mut root: Box<Self>) -> Box<Self> {
+        assert!(root.right.is_some());
+        let mut new_root = root.right.take().unwrap();
+        assert!(root.right.is_none());
+        root.right = new_root.left.take();
+        Self::update(&mut root);
+        new_root.left = Some(root);
+        Self::update(&mut new_root);
+        new_root
+    }
+
+    pub(crate) fn rotate_right(mut root: Box<Self>) -> Box<Self> {
+        assert!(root.left.is_some());
+        let mut new_root = root.left.take().unwrap();
+        assert!(root.left.is_none());
+        root.left = new_root.right.take();
+        Self::update(&mut root);
+        new_root.right = Some(root);
+        Self::update(&mut new_root);
+        new_root
+    }
+
+    pub(crate) fn splay(root: Option<Box<Self>>, key: &K) -> Option<Box<Self>> {
+        if root.is_none() {
+            return None;
+        }
+        let mut root = root.unwrap();
+        if &root.key == key {
+            return Some(root);
+        }
+        if key < &root.key {
+            if root.left.is_none() {
+                return Some(root);
+            }
+            let mut left = root.left.take().unwrap();
+            if key < &left.key && left.left.is_some() {
+                left.left = Self::splay(left.left, key);
+                root.left = Some(left);
+                root = Self::rotate_right(root);
+            } else if key > &left.key && left.right.is_some() {
+                left.right = Self::splay(left.right, key);
+                left = Self::rotate_left(left);
+                root.left = Some(left);
+            }
+            root = Self::rotate_right(root);
+        } else {
+            if root.right.is_none() {
+                return Some(root);
+            }
+            let mut right = root.right.take().unwrap();
+            if key < &right.key && right.left.is_some() {
+                right.left = Self::splay(right.left, key);
+                right = Self::rotate_right(right);
+                root.right = Some(right);
+            } else if key > &right.key && right.right.is_some() {
+                right.right = Self::splay(right.right, key);
+                root.right = Some(right);
+                root = Self::rotate_left(root);
+            }
+            root = Self::rotate_left(root);
+        }
+        Some(root)
+    }
+
+
 }
 
 // @dataclasses.dataclass
 // class SplayTreeMap(typing.Generic[T, V]):
 //     root: typing.Optional[Node[T, V]] = None
-
-//     def rotate(self) -> None:
-//         u = self.root
-//         if self.__key < u.key:
-//             v = u.left
-//             u.left = v.right
-//             v.right = u
-//         else:
-//             v = u.right
-//             u.right = v.left
-//             v.left = u
-//         self.root = v
-
-//     def splay(self, key: T) -> None:
-//         self.__key = key
-//         us = self.__state()
-//         if not us:
-//             return
-//         u = self.root
-//         v = u.left if us < 0 else u.right
-//         self.root = v
-//         vs = self.__state()
-//         if not vs:
-//             self.root = u
-//             self.rotate()
-//             return
-//         self.root = v.left if vs < 0 else v.right
-//         self.splay(key)
-//         if vs < 0:
-//             v.left = self.root
-//         else:
-//             v.right = self.root
-//         if us == vs:
-//             self.root = u
-//             self.rotate()
-//         else:
-//             self.root = v
-//             self.rotate()
-//             if us < 0:
-//                 u.left = self.root
-//             else:
-//                 u.right = self.root
-//             self.root = u
-//         self.rotate()
 
 //     def __state(self) -> int:
 //         u = self.root
