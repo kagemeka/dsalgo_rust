7094310bd5e5455ec925585d71ba6bef2da266d6
kagemeka <kagemeka1@gmail.com>
2022-05-10 00:50:18 +0900

    update io

--- src/io.rs
+++ src/io.rs
@@ -1,10 +1,10 @@
-fn read<R, T>(reader: &mut R) -> Result<T, <T as std::str::FromStr>::Err>
+fn read_stdin<T>() -> Result<T, <T as std::str::FromStr>::Err>
 where
-    R: std::io::Read,
     T: std::str::FromStr,
 {
     use std::io::Read;
-    reader
+    std::io::stdin()
+        .lock()
         .by_ref()
         .bytes()
         .map(|c| c.unwrap() as char)
@@ -14,41 +14,45 @@ where
         .parse::<T>()
 }
 
-pub struct ReadWrapper<R: std::io::Read> {
+pub struct ReadWrapper<R: std::io::BufRead> {
     reader: R,
+    tokens: Vec<String>,
 }
 
-impl<R: std::io::Read> ReadWrapper<R> {
+impl<R: std::io::BufRead> ReadWrapper<R> {
     /// let stdin = std::io::stdin();
-    /// let mut reader = ReadWrapper::new(std::io::BufReader::new(stdin.lock()));
+    /// let mut reader = ReadWrapper::new(stdin.lock());
     /// let x = reader.read::<usize>();
-    pub fn new(reader: R) -> Self { Self { reader: reader } }
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            tokens: vec![],
+        }
+    }
 
     pub fn read<T: std::str::FromStr>(&mut self) -> Result<T, <T as std::str::FromStr>::Err> {
-        read(&mut self.reader)
+        while self.tokens.is_empty() {
+            let mut buf = String::new();
+            self.reader.read_line(&mut buf);
+            self.tokens = buf.split_whitespace().rev().map(String::from).collect();
+        }
+        self.tokens.pop().unwrap().parse::<T>()
     }
 }
 
-pub fn locked_buf_reader() -> ReadWrapper<std::io::BufReader<std::io::StdinLock<'static>>> {
+pub fn locked_stdin_reader() -> ReadWrapper<std::io::StdinLock<'static>> {
     let stdin = Box::leak(Box::new(std::io::stdin()));
-    ReadWrapper::new(std::io::BufReader::new(stdin.lock()))
+    ReadWrapper::new(stdin.lock())
 }
 
 /// Example
 /// ```
 /// use std::io::Write;
-/// let writer = &mut locked_buf_writer();
+/// let mut writer = locked_buf_writer();
 /// writeln!(writer, "Hello, world!");
 /// writer.flush().unwrap();
 /// ```
-pub fn locked_buf_writer() -> std::io::BufWriter<std::io::StdoutLock<'static>> {
+pub fn locked_stdin_buf_writer() -> std::io::BufWriter<std::io::StdoutLock<'static>> {
     let stdout = Box::leak(Box::new(std::io::stdout()));
     std::io::BufWriter::new(stdout.lock())
-}
-
-pub fn fast_io() -> (
-    ReadWrapper<std::io::BufReader<std::io::StdinLock<'static>>>,
-    std::io::BufWriter<std::io::StdoutLock<'static>>,
-) {
-    (locked_buf_reader(), locked_buf_writer())
 }
