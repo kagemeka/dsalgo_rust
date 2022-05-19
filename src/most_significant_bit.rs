574fedeb5368fe62bb52c024c01cbbc0319960a6
kagemeka <kagemeka1@gmail.com>
2022-05-19 10:59:24 +0000

    refactor lsb

--- src/most_significant_bit.rs
+++ src/most_significant_bit.rs
@@ -2,7 +2,7 @@ use crate::bit_length::bit_length;
 
 pub fn msb(n: u64) -> usize {
     assert!(n > 0);
-    // it's trivial msb of 0 is undefined.
+    // it's trivial that msb of 0 is undefined.
     // if n = 0, it's wrong with the caller.
     bit_length(n) as usize - 1
 }
