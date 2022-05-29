49d02f60750f973f2e856007571d3f34712b12c2
kagemeka <kagemeka1@gmail.com>
2022-05-29 10:28:31 +0000

    test fact/invfact table

--- src/factorial_table.rs
+++ src/factorial_table.rs
@@ -12,3 +12,25 @@ where
     let op = |a: T, b: T| -> T { a * b };
     accumulate(&op, v.into_iter()).collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {
+        use crate::{modular::Modular, static_modulus::StaticMod};
+        type Mint = Modular<StaticMod<1_000_000_007>>;
+        let res = factorial_table::<Mint>(20)
+            .into_iter()
+            .map(|x| x.value())
+            .collect::<Vec<u32>>();
+        assert_eq!(
+            res,
+            [
+                1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800,
+                39916800, 479001600, 227020758, 178290591, 674358851,
+                789741546, 425606191, 660911389, 557316307
+            ],
+        );
+    }
+}
