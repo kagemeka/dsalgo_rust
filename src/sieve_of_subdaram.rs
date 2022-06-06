e8543a988815c4037d50db15176e9a3786fe8d9f
kagemeka <kagemeka1@gmail.com>
2022-06-06 00:26:25 +0000

    optimize sieve of sundaram

--- src/sieve_of_subdaram.rs
+++ src/sieve_of_subdaram.rs
@@ -1,25 +1,17 @@
 pub fn sieve_of_sundaram(less_than: u64) -> Vec<u64> {
+    let mut prime_numbers = vec![];
+    if less_than <= 2 {
+        return prime_numbers;
+    }
+    prime_numbers.push(2);
     let size = (less_than >> 1) as usize;
     let mut is_prime = vec![true; size];
     for i in 1..size {
-        if 2 * i * (i + 1) >= size {
-            break;
-        }
-        for j in i..size {
-            let j = i + j + 2 * i * j;
-            if j >= size {
-                break;
-            }
-            is_prime[j] = false;
-        }
-    }
-    let mut prime_numbers = vec![];
-    if less_than > 2 {
-        prime_numbers.push(2);
-    }
-    for i in 1..size {
         if is_prime[i] {
-            prime_numbers.push((2 * i + 1) as u64);
+            prime_numbers.push(((i as u64) << 1) | 1);
+        }
+        for j in (i * (i + 1) << 1..size).step_by((i << 1) | 1) {
+            is_prime[j] = false;
         }
     }
     prime_numbers
@@ -31,9 +23,12 @@ mod tests {
     fn test() {
         use super::*;
         use crate::sieve_of_eratosthenes::sieve_of_eratosthenes;
-        assert_eq!(
-            sieve_of_sundaram(100000),
-            sieve_of_eratosthenes(100000),
-        );
+        let limits = [99, 100, 101, 102, 1 << 17];
+        for limit in limits {
+            assert_eq!(
+                sieve_of_sundaram(limit),
+                sieve_of_eratosthenes(limit as usize),
+            );
+        }
     }
 }
